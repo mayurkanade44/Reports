7df8f472e4481a3e3d5cbcdc4a2b06718e1e872a
mayurkanade44 <mayurkanade44@gmail.com>
2023-01-31 18:39:54 +0530

    redux setup

---
+++ client/src/redux/reportSlice.jsx
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  loading: false,
+  images: [],
+};
+
+export const createReport = createAsyncThunk(
+  "report/create",
+  async (form, thunkAPI) => {
+    try {
+      console.log(form);
+      //   const res = await axios.post("/report/create");
+      //   return res.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
+export const uploadImage = createAsyncThunk(
+  "report/imgUpload",
+  async (form, thunkAPI) => {
+    try {
+      const res = await axios.post("/api/report/uploadImage", form);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+    }
+  }
+);
+
+const reportSlice = createSlice({
+  name: "report",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(createReport.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(createReport.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        console.log(payload.msg);
+      })
+      .addCase(createReport.rejected, (state, { payload }) => {
+        state.loading = false;
+        console.log(payload);
+      })
+      .addCase(uploadImage.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(uploadImage.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        console.log(payload);
+      })
+      .addCase(uploadImage.rejected, (state, { payload }) => {
+        state.loading = false;
+        console.log(payload);
+      });
+  },
+});
+
+export default reportSlice.reducer;
