274250df0fdc37f9faf36ffb117a6eebcb8d80bf
mayurkanade44 <mayurkanade44@gmail.com>
2023-03-11 18:30:24 +0530

    bug fix

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -422,11 +422,13 @@ export const sendEmail = async (req, res) => {
       emailTo = emailTo.concat(emails.split(","));
     }
 
+   
+
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: emailTo,
       cc: ["sales@epcorn.com", "natco.epcorn@gmail.com"],
-      from: { email: "noreply.epcorn@gmail.com", name: "Epcorn" },
+      from: { email: "noreply.epcorn@gmail.com", name: "Epcorn Reports" },
       dynamic_template_data: {
         fileName: report.reportName,
         name: req.user.name,
